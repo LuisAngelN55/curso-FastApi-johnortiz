8a1b1c2a4048a927eedaa24d479d60bb6859ec51
Luis Angel <lang@zuntrix.com>
2023-06-28 23:27:44 -0500

    clase 14

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from typing import Union
+from models.item_model import Item
+
 
 app = FastAPI()
 
@@ -7,6 +9,7 @@ app = FastAPI()
 @app.get('/')
 def readRoot():
     return { "Hello": "World"}
+
 
 @app.get('/hola')
 def readRoot():
@@ -19,3 +22,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get('/calculadora')
 def calcular(operando1: float, operando2: float):
     return { 'suma': operando1 + operando2 }
+
+
+@app.put('/{item_id}')
+def update_item(intem_id: int, item: Item):
+    return { 'item_name': item.name, 'item_id': intem_id, 'item_price': item.price }
