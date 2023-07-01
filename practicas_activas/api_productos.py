e4f919fd52bcfc39b22219922f9539be1dc51fcb
Luis Angel <lang@zuntrix.com>
2023-06-30 23:28:23 -0500

    clase 39

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from uuid import uuid4 as uuid
 from typing import Optional
 
 
@@ -13,6 +14,28 @@ class Producto(BaseModel):
 
 app = FastAPI()
 
+productos = []
+
 @app.get('/')
 def index():
     return { "mensaje": "Bienvenido a la api de productos" }
+
+@app.get('/productos')
+def obtener_producto():
+    return productos
+
+
+@app.post('/productos')
+def crear_producto(producto: Producto):
+    producto.id = str(uuid())
+    productos.append(producto)
+    return { 'mensaje': 'Producto creado satisfactoriamente'}
+
+
+@app.get('/productos/{producto_id}')
+def obtener_producto_por_id(producto_id: str):
+    for p in productos:
+        if p.id == producto_id:
+            return p
+    
+    return {'mensaje': f'El producto con el ID {producto_id} no fue encontrado'}
